ebe6548448618eaa256255e0c8a9fa7222746128
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-11-11 12:31:41 +0100

    handle changes inputs ifs

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
-import React, { FC, useState } from "react";
+import React, { ChangeEvent, FC, useState } from "react";
 import "./App.css";
 const App: FC = () => {
   const [task, setTask] = useState<string>("");
   const [deadline, setDeadline] = useState<number>(0);
   const [todo, setTodo] = useState([]);
-
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    event.target.name === "task"
+      ? setTask(event.target.value)
+      : setDeadline(parseInt(event.target.value));
+  };
   return (
     <div className="App">
       <div className="header">
         <div className="inputContainer">
-          <input type="text" placeholder="Task..." />
-          <input type="number" placeholder="Deadline..." />
+          <input
+            type="text"
+            placeholder="Task..."
+            name="task"
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            placeholder="Deadline..."
+            name="deadline"
+            onChange={handleChange}
+          />
         </div>
         <button>Add task</button>
       </div>
