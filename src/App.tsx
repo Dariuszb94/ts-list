345f84fb9f8d4f035bf70897d92e5727fcdc3e2b
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-11-11 08:48:14 +0100

    structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import React, { FC } from "react";
 import "./App.css";
-
-function App() {
-  return <div className="App"></div>;
-}
+const App: FC = () => {
+  return (
+    <div className="App">
+      <div className="header"></div>
+      <div className="todoList"></div>
+    </div>
+  );
+};
 
 export default App;
